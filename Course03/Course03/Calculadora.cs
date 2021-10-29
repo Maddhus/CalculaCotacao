b45bc5e8362eb556138c038adede6dd5c585e40e
Maria Eduarda <60519406+Maddhus@users.noreply.github.com>
2021-10-29 10:43:46 -0300

    Add files via upload

---
+++ Course03/Course03/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization; 
+
+namespace Course03
+{
+    class Calculadora
+    {
+        public static double Iof = 6.0;
+
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100.00;
+        }
+
+    }
+}
---
+++ Course03/Course03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course03
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double result = Calculadora.DolarParaReal(quantia, cotacao);
+
+
+            Console.WriteLine("Valor a ser pago em Reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+
+       
+    }
+}
